453fb1c547301058c5d39ea8f150e7e49f0c16ac
月杪 <moonkinstar@gmail.com>
2017-06-05 11:19:58 +0800

    Update general file

--- general.sh
+++ general.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+rm referer_spam.conf
 sort referrer-spam-blacklist/spammers.txt | uniq | sed 's/\./\\\./g' | while read host;
 do
 	echo "if (\$http_referer ~ '$host') {return 403;}" >> referer_spam.conf
